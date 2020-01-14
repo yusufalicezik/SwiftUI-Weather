dd5c24f7b6486b04d25e35ecef71e86bc38bb8e7
yusufalicezik <yusufalicezik35@gmail.com>
2020-01-14 19:09:26 +0300

    Initial Commit

---
+++ SwiftUI-Weather/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Weather
+//
+//  Created by Yusuf ali cezik on 14.01.2020.
+//  Copyright © 2020 Yusuf Ali Cezik. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
